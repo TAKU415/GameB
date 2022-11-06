55d8b7a5619fdf8f979cdb1bdfc3df8e3e278fef
TAKU415 <th.mzk415@gmail.com>
2022-11-06 14:51:02 +0900

    a

--- Project/GameProject/Game/Player.cpp
+++ Project/GameProject/Game/Player.cpp
@@ -1,5 +1,6 @@
 #include "Player.h"
 #include "Bullet1.h"
+#include "GameData.h"
 
 Player::Player(const CVector2D& pos) : Base(eType_Player) {
 	m_img.Load("Image/Player.png");
@@ -35,7 +36,7 @@ void Player::Update() {
 	m_ang = atan2(vec.x, vec.y);
 	
 	//’e
-	if (HOLD(CInput::eMouseL)) {
+	if (HOLD(CInput::eMouseL) && GameData::s_score <= 2000) {
 		if (m_cnt >= 4) {
 			//SOUND("SE_Hit01")->Play();
 			Base::Add(new Bullet1(eType_Player_Attack, m_pos, m_ang, 4));
